af9092d43a0a1a967ca94a690b12b72871107ef1
fibber2240 <96056157+fibber2240@users.noreply.github.com>
2023-08-03 20:01:57 +0300

    Функциональный интерфейс (тренировка)

--- src/lambda/StudentInfo.java
+++ src/lambda/StudentInfo.java
@@ -1,6 +1,8 @@
 package lambda;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class StudentInfo {
     void testStudents(ArrayList<Student> al, StudentChecks sc) {
@@ -48,6 +50,14 @@ public class StudentInfo {
         students.add(st5);
 
            StudentInfo info = new StudentInfo();
+//        Collections.sort(students, new Comparator<Student>() {
+//            @Override
+//            public int compare(Student o1, Student o2) {
+//                return o1.age - o2.age;
+//            }
+//        });
+        Collections.sort(students, (s1,s2) -> s1.age - s2.age);
+        System.out.println(students);
 //        info.testStudents(students, new CheckOverAge());
 //        System.out.println("-------------------------------------");
 //        // анонимный класс
@@ -57,12 +67,18 @@ public class StudentInfo {
 //                return s.age < 35;
 //            }
 //        });
-        info.testStudents(students, (Student s) -> {return s.age > 30 && s.course > 2;});
-        System.out.println("-------------------------------------");
-        info.testStudents(students, (Student s) -> {return s.course > 3;});
-        System.out.println("-------------------------------------");
-        // лямбда выражение (сложная запись)
-        info.testStudents(students, (Student s) -> {return s.age < 35;});
+//        info.testStudents(students, (Student s) -> {return s.age > 30 && s.course > 2;});
+//        System.out.println("-------------------------------------");
+//        info.testStudents(students, (Student s) -> {return s.course > 3;});
+//        System.out.println("-------------------------------------");
+//        // лямбда выражение (полная запись)
+//        info.testStudents(students, (Student s) -> {return s.age < 35;});
+//        // лямбда выражение (сокращенная версия) с указание Типа параметра
+//        info.testStudents(students, (Student s) -> s.age < 35);
+//        // таже запись, но без указания типа параметра , поэтому нет ()
+//        info.testStudents(students, s -> {return s.age < 35;});
+//        // самый сокращенный вариант написания этого выражения
+//        info.testStudents(students, s-> s.age < 35);
 
 //        info.printStudentsMixConditions(students,5,3);
 //        System.out.println("-------------------------------------");
@@ -76,9 +92,8 @@ interface StudentChecks {
     boolean check(Student s);
 }
 
-class CheckOverAge implements StudentChecks {
-    @Override
-    public boolean check(Student s) {
-        return s.age > 35;
-    }
-}
+//class CheckOverAge implements StudentChecks {
+//    @Override
+//    public boolean check(Student s) {
+//        return s.age > 35;
+//    }
